b7645136d36e4cca6abdb5e9c6c6f20998242adb
Danielle Castro <cdl0923@dlsud.edu.ph>
2024-09-10 11:05:58 +0800

    added tab bar

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -86,133 +86,160 @@ class _CategoryScreenState extends State<CategoryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/wealthcast_logo.png',
-              height: 40,
-            ),
-            const SizedBox(width: 10),
-            const Text(
-              'Wealthcast',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.green,
+    return DefaultTabController(
+      length: 4, // We have 4 tabs
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/wealthcast_logo.png',
+                height: 40,
               ),
+              const SizedBox(width: 10),
+              const Text(
+                'Wealthcast',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Income'),
+              Tab(text: 'Expenses'),
+              Tab(text: 'Predictions'),
+              Tab(text: 'Settings'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            // Income Tab
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                        context: context,
+                        isScrollControlled: true,
+                        builder: (_) => const IncomeForm(),
+                      );
+                    },
+                    child: const Text('Add Income'),
+                  ),
+                  const SizedBox(height: 20), // Space between buttons
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(IncomeScreen.name);
+                    },
+                    child: const Text('View All Income'),
+                  ),
+                ],
+              ),
+            ),
+
+            // Expenses Tab (Current content moved here)
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Consumer<DatabaseProvider>(
+                    builder: (context, dbProvider, child) {
+                      return Row(
+                        children: [
+                          // Display Expense Type and Value
+                          Expanded(
+                            child: GestureDetector(
+                              onTap: _toggleExpenseType, // Toggle the expense type on tap
+                              child: Container(
+                                padding: const EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200], // Light grey background
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      _currentExpenseType,
+                                      style: const TextStyle(
+                                        fontSize: 12, // Smaller font size
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 4), // Small space between the label and value
+                                    Text(
+                                      '₱ ${_currentExpenseValue.toStringAsFixed(2)}',
+                                      style: const TextStyle(
+                                        fontSize: 24, // Larger font size for the value
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 4), // Space between value and date range
+                                    Text(
+                                      _currentExpenseDateRange,
+                                      style: const TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.normal,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 16), // Space between container and calendar icon
+                          // Calendar Icon for Date Picker
+                          IconButton(
+                            onPressed: _pickDate, // Opens the date picker
+                            icon: const Icon(Icons.calendar_today),
+                            tooltip: 'Pick a date',
+                          ),
+                          const SizedBox(width: 16), // Space between calendar icon and button
+                          ElevatedButton(
+                            onPressed: () {
+                              showModalBottomSheet(
+                                context: context,
+                                isScrollControlled: true,
+                                builder: (_) => const ExpenseForm(),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.all(10.0),
+                              shape: const CircleBorder(),
+                              backgroundColor: Colors.green, // Updated parameter
+                            ),
+                            child: const Icon(Icons.add, color: Colors.white),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                const Expanded(child: CategoryFetcher()), // Ensure this is expanded to take up available space
+              ],
+            ),
+
+            // Predictions Tab
+            Center(
+              child: Text('Predictions Content'),
+            ),
+
+            // Settings Tab
+            Center(
+              child: Text('Settings Content'),
             ),
           ],
         ),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Consumer<DatabaseProvider>(
-              builder: (context, dbProvider, child) {
-                return Row(
-                  children: [
-                    // Display Expense Type and Value
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: _toggleExpenseType, // Toggle the expense type on tap
-                        child: Container(
-                          padding: const EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                            color: Colors.grey[200], // Light grey background
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                _currentExpenseType,
-                                style: const TextStyle(
-                                  fontSize: 12, // Smaller font size
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(height: 4), // Small space between the label and value
-                              Text(
-                                '₱ ${_currentExpenseValue.toStringAsFixed(2)}',
-                                style: const TextStyle(
-                                  fontSize: 24, // Larger font size for the value
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(height: 4), // Space between value and date range
-                              Text(
-                                _currentExpenseDateRange,
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.normal,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 16), // Space between container and calendar icon
-                    // Calendar Icon for Date Picker
-                    IconButton(
-                      onPressed: _pickDate, // Opens the date picker
-                      icon: const Icon(Icons.calendar_today),
-                      tooltip: 'Pick a date',
-                    ),
-                    const SizedBox(width: 16), // Space between calendar icon and button
-                    ElevatedButton(
-                      onPressed: () {
-                        showModalBottomSheet(
-                          context: context,
-                          isScrollControlled: true,
-                          builder: (_) => const ExpenseForm(),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.all(10.0),
-                        shape: const CircleBorder(),
-                        backgroundColor: Colors.green, // Updated parameter
-                      ),
-                      child: const Icon(Icons.add, color: Colors.white),
-                    ),
-                  ],
-                );
-              },
-            ),
-          ),
-          const Expanded(child: CategoryFetcher()), // Ensure this is expanded to take up available space
-        ],
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          const SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: () {
-              showModalBottomSheet(
-                context: context,
-                isScrollControlled: true,
-                builder: (_) => const IncomeForm(),
-              );
-            },
-            heroTag: 'income',
-            child: const Icon(Icons.attach_money),
-          ),
-          const SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed(IncomeScreen.name);
-            },
-            heroTag: 'view_income',
-            child: const Icon(Icons.account_balance_wallet),
-          ),
-        ],
       ),
     );
   }
